d674adb3ad4ce2b64be34ddb31beb35841b1d40e
lehag7 <81866942+lehag7@users.noreply.github.com>
2021-05-10 19:29:55 -0500

    Create CheckAdminRights.ps1

---
+++ Scripts/CheckAdminRights.ps1
@@ -0,0 +1,7 @@
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if (($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+    Write-Host "You are running as the administrator."
+    Exit 1
+}
+
+Write-Host "This script needs to be running as the administrator."
